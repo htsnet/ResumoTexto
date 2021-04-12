a22e81f7713cefc6aab1295cfaf49f96d622d177
Your NameHamilton <hamilton@hts-net.com.br>
2021-04-12 11:05:45 -0300

    melhorias e início tradução

--- app.py
+++ app.py
@@ -23,8 +23,26 @@ import pandas as pd
 
 import altair as alt
 
-# Configuração inicial da página
-st.set_page_config(page_title='NLP: Resumo de Textos', page_icon='favicon.ico', layout='centered', )
+import json
+
+# valores iniciais ou fixos
+currenteCodeLanguage = 'pt'
+language = ['Português', 'English']
+codes = {'português': 'pt', 'english': 'en'}
+
+with open('translation.json', 'r', encoding='UTF-8') as j:
+    json_dictionary = json.load(j)
+    # print(json_dictionary)
+    # print(json_dictionary['pt']['nome'])
+
+def translate(key):
+    global currenteCodeLanguage
+    # print(currenteCodeLanguage)
+    try:
+        return json_dictionary[currenteCodeLanguage][key]
+    except:
+        return key
+
 
 #para esconder o menu do próprio streamlit 
 hide_streamlit_style = """
@@ -53,57 +71,41 @@ def evaluate_summary(summary,reference):
     eval_score_df = pd.DataFrame(eval_score[0])
     return eval_score_df
 
-def main():
-    """
-    A Simple Summarization NLP App
-    """
+# Configuração inicial da página
+st.set_page_config(page_title=translate('titulo'), page_icon='favicon.png', layout='centered', )
 
-    st.title('Summarizer App')
+def main():
+    global currenteCodeLanguage
+    
+    currentLanguage = st.radio(translate('escolha'), language).lower()
+    currenteCodeLanguage = codes[currentLanguage]
+
+    st.title(translate('titulo'))
     menu = ['Home', 'About']
     choice = st.sidebar.selectbox('Menu', menu)
     
     if choice == 'Home':
-        st.subheader('Summarization')
+        st.subheader(translate('resumo'))
 
-        language = ['Portuguese', 'English']
-        currentLanguage = st.selectbox('Language', language).lower()
+        raw_text = st.text_area(translate('cole'), height=50)
+        textLanguage = st.radio(translate('escolhaTexto'), language).lower()
+        qtdeFrases = st.slider(translate('qtdeFrases'), 3, 200, 5) 
 
-        raw_text = st.text_area('Enter Text Here')
-        if st.button('Summarize'):
+        #só mostra se tem conteúdo
+    
+        if st.button(translate('executar')):
+            # só executa se tiver texto
+            if len(raw_text) > 0:
             
-            with st.beta_expander('Original Text'):
-                st.write(raw_text)
+                with st.beta_expander('Original Text'):
+                    st.write(raw_text)
 
-            # layout
-            c1, c2 = st.beta_columns(2)
+                # layout
+                c1, c2 = st.beta_columns(2)
 
-            with c1:
-                with st.beta_expander('LexRank Summary'):
-                    my_summary = sumy_summarizer(raw_text, currentLanguage, 5)
-                    document_len = {
-                        "Original": len(raw_text), 
-                        'Summary': len(my_summary)
-                        }
-                    st.write(document_len)
-                    st.write(my_summary)
-
-                    st.info("Rouge Score")
-                    eval_df = evaluate_summary(my_summary, raw_text)
-                    st.dataframe(eval_df.T)
-                    eval_df['metrics'] = eval_df.index
-                    c = alt.Chart(eval_df).mark_bar().encode(
-                        x = 'metrics',
-                        y = 'rouge-1'
-                    )
-                    st.altair_chart(c)
-
-                    
-
-            with c2:
-                with st.beta_expander('TextRank Summary'):
-                    # para o caso de texto muito pequeno
-                    try:
-                        my_summary = summarize(raw_text)
+                with c1:
+                    with st.beta_expander('LexRank Summary'):
+                        my_summary = sumy_summarizer(raw_text, textLanguage, qtdeFrases)
                         document_len = {
                             "Original": len(raw_text), 
                             'Summary': len(my_summary)
@@ -113,17 +115,42 @@ def main():
 
                         st.info("Rouge Score")
                         eval_df = evaluate_summary(my_summary, raw_text)
-                        st.dataframe(eval_df)
+                        st.dataframe(eval_df.T)
                         eval_df['metrics'] = eval_df.index
                         c = alt.Chart(eval_df).mark_bar().encode(
                             x = 'metrics',
                             y = 'rouge-1'
                         )
                         st.altair_chart(c)
-                    except:
-                        st.write(raw_text)
-                        st.info("Texto original mantido")
 
+                        
+
+                with c2:
+                    with st.beta_expander('TextRank Summary'):
+                        # para o caso de texto muito pequeno
+                        try:
+                            my_summary = summarize(raw_text)
+                            document_len = {
+                                "Original": len(raw_text), 
+                                'Summary': len(my_summary)
+                                }
+                            st.write(document_len)
+                            st.write(my_summary)
+
+                            st.info("Rouge Score")
+                            eval_df = evaluate_summary(my_summary, raw_text)
+                            st.dataframe(eval_df)
+                            eval_df['metrics'] = eval_df.index
+                            c = alt.Chart(eval_df).mark_bar().encode(
+                                x = 'metrics',
+                                y = 'rouge-1'
+                            )
+                            st.altair_chart(c)
+                        except:
+                            st.write(raw_text)
+                            st.info("Texto original mantido")
+            else:
+                st.error(translate('vazio'))
     else:
         st.subheader('About')
 
